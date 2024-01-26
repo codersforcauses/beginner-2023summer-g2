42331b5ecd4c5a046454b3504b9e753f32e84341
Cindy.N <nguyencindy2011@gmail.com>
2024-01-26 14:13:21 +0800

    character move winthin the border

--- gamepage.js
+++ gamepage.js
@@ -12,18 +12,40 @@ let y = 70
 let size = 5
 
 function character(){
+
     //draw character
     ctx.fillStyle ='white'
     ctx.fillRect(x, y, size, size)
 
-
 }
+function moveCharacter(key) {
+    // Adjust character position based on arrow keys
+    switch (key.keyCode) {
+      case 37: // Left arrow key
+        if (x > 0) x -= 2;
+        break;
+      case 38: // Up arrow key
+        if (y > 0) y -= 2;
+        break;
+      case 39: // Right arrow key
+        if (x < canvasWidth - size) x += 2;
+        break;
+      case 40: // Down arrow key
+        if (y < canvasHeight - size) y += 2;
+        break;
+    }
+    key.preventDefault();
+  }
+
+  // Event listener for arrow key input
+  window.addEventListener("keydown", moveCharacter,false);
+
 // enemies position
 let enemiesX = 20
 let enemiesY = 20
 //enemies size
 const enemiesWidth = 10
-const enemiesHeight = 5
+const enemiesHeight = 10
 
 
 function enemies(){
@@ -35,7 +57,6 @@ function enemies(){
 function draw(){
     ctx.fillStyle ='grey'
     ctx.fillRect(0, 0, canvasWidth, canvasHeight)
-
 
     enemies()
     character()
